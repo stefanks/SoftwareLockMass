3db0ec645fd415d85f2aa39d9bf3d213f8b85647
stefanks <solntsev@gmail.com>
2016-05-09 14:02:51 -0500

    Better clustering

--- SoftwareLockMass/CalibrationFunctionClustering.cs
+++ SoftwareLockMass/CalibrationFunctionClustering.cs
@@ -8,54 +8,55 @@ namespace SoftwareLockMass
     internal class CalibrationFunctionClustering
     {
         private int numClusters;
-        private double[] CentroidMZ;
-        private double[] CentroidTime;
-        private double[] errorAtCluster;
+        private double[] CentroidMZfinal;
+        private double[] CentroidTimefinal;
+        private double[] errorAtClusterfinal;
 
-        public CalibrationFunctionClustering(List<double[]> trainingData, List<double> labelData, int numClusters = 5)
+        public CalibrationFunctionClustering(List<double[]> trainingData, List<double> labelData, int numClusters = 3)
         {
             this.numClusters = numClusters;
             int numTp = labelData.Count;
-            bool changeHappend = true;
-            var Cluster = Enumerable.Repeat(0, numTp).ToArray();
-            errorAtCluster = Enumerable.Repeat(0.0, numClusters).ToArray();
+            Random randNum = new Random();
+            double totalError = double.MaxValue;
 
-            CentroidMZ = new double[]
-{
-        400,
-        400,
-        900,
-        1400,
-        1400
-};
-            CentroidTime = new double[]
-{
-        30,
-        170,
-        100,
-        170,
-        30
-};
-            while (changeHappend)
+            for (int numTries = 0; numTries < 10000; numTries++)
             {
-                changeHappend = false;
+                bool changeHappend = true;
+                var Cluster = Enumerable.Repeat(0, numTp).ToArray();
+                double[] errorAtCluster = Enumerable.Repeat(0.0, numClusters).ToArray();
+                double[] CentroidMZ = new double[numClusters] ;
+                double[] CentroidTime = new double[numClusters];
                 int[] PointsInCluster = Enumerable.Repeat(0, numClusters).ToArray();
-
-                for (int i = 0; i < numTp; i++)
+                double[] mseInCluster = Enumerable.Repeat(0.0, numClusters).ToArray();
+                for (int i = 0; i < numClusters; i++)
                 {
-                    double bestDistance = double.MaxValue;
-                     var oldClusterI = Cluster[i];
-                    for (int j = 0; j < numClusters; j++)
-                    {
-                        if (dist(trainingData[i][1], trainingData[i][2], j) < bestDistance)
-                        {
-                            bestDistance = dist(trainingData[i][1], trainingData[i][2], j);
-                            Cluster[i] = j;
-                        }
-                    }
-                    if (Cluster[i] != oldClusterI)
-                        changeHappend = true;
+                    var kk = randNum.Next(numTp);
+                    //Console.WriteLine(kk);
+                    CentroidMZ[i] = trainingData[kk][1];
+                    CentroidTime[i] = trainingData[kk][2];
                 }
+                while (changeHappend)
+                {
+                    changeHappend = false;
+
+                    for (int i = 0; i < numTp; i++)
+                    {
+                        double bestDistance = double.MaxValue;
+                        var oldClusterI = Cluster[i];
+                        for (int j = 0; j < numClusters; j++)
+                        {
+                            if (dist(trainingData[i][1], trainingData[i][2], j, CentroidMZ, CentroidTime) < bestDistance)
+                            {
+                                bestDistance = dist(trainingData[i][1], trainingData[i][2], j, CentroidMZ, CentroidTime);
+                                Cluster[i] = j;
+                            }
+                        }
+                        if (Cluster[i] != oldClusterI)
+                            changeHappend = true;
+                    }
+                 }
+
+                // Settled in on class for each point by now
                 for (int i = 0; i < numTp; i++)
                 {
                     CentroidMZ[Cluster[i]] += trainingData[i][1];
@@ -69,13 +70,29 @@ namespace SoftwareLockMass
                     CentroidTime[j] = CentroidTime[j] / PointsInCluster[j];
                     errorAtCluster[j] = errorAtCluster[j] / PointsInCluster[j];
                 }
-
+                for (int i = 0; i < numTp; i++)
+                {
+                    mseInCluster[Cluster[i]] += (labelData[i] - errorAtCluster[Cluster[i]]) * (labelData[i] - errorAtCluster[Cluster[i]]);
+                }
+                
+                if (totalError > mseInCluster.Sum())
+                {
+                    totalError = mseInCluster.Sum();
+                    //Console.WriteLine("totalMSE" + totalError);
+                    //for (int j = 0; j < numClusters; j++)
+                    //{
+                    //    Console.WriteLine(CentroidMZ[j] + " " + CentroidTime[j] + " " + errorAtCluster[j]);
+                    //}
+                    CentroidMZfinal = (double[]) CentroidMZ.Clone();
+                    CentroidTimefinal=(double[])CentroidTime.Clone();
+                    errorAtClusterfinal=(double[])errorAtCluster.Clone();
+                }
             }
         }
 
-        private double dist(double v1, double v2, int j)
+        private double dist(double v1, double v2, int j, double[] CentroidMZdist, double[] CentroidTimedist)
         {
-            return (v1 - CentroidMZ[j]) * (v1 - CentroidMZ[j]) + (v2 - CentroidTime[j]) * (v2 - CentroidTime[j]);
+            return (v1 - CentroidMZdist[j]) * (v1 - CentroidMZdist[j]) + (v2 - CentroidTimedist[j]) * (v2 - CentroidTimedist[j]);
         }
 
         internal double calibrate(double mz, double retentionTime)
@@ -84,13 +101,13 @@ namespace SoftwareLockMass
             double bestDistance = double.MaxValue;
             for (int j = 0; j < numClusters; j++)
             {
-                if (dist(mz, retentionTime, j) < bestDistance)
+                if (dist(mz, retentionTime, j, CentroidMZfinal, CentroidTimefinal) < bestDistance)
                 {
-                    bestDistance = dist(mz, retentionTime, j);
+                    bestDistance = dist(mz, retentionTime, j, CentroidMZfinal, CentroidTimefinal);
                     theCluster = j;
                 }
             }
-            return errorAtCluster[theCluster];
+            return errorAtClusterfinal[theCluster];
         }
     }
 }
