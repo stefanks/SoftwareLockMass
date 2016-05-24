8aa3a2cde7bd386349b105fb1ed8e22c73e32242
stefanks <solntsev@gmail.com>
2016-05-24 12:13:56 -0500

    Add test

---
+++ Test/TestSoftwareLockMass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using SoftwareLockMass;
+
+namespace Test
+{
+    [TestFixture]
+    public sealed class TestSoftwareLockMass
+    {
+        [Test]
+        public void DefaultOutputFileCheck()
+        {
+            SoftwareLockMassParams p = new SoftwareLockMassParams("a.mzML", "b.mzid");
+            Assert.AreEqual("a-Calibrated.mzML", p.outputFile);
+        }
+    }
+}
