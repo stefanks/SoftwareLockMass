d77044c2a74e9bc7b3683a9d6d15c6853ea87db3
stefan <solntsev@wisc.edu>
2016-06-14 15:06:47 -0500

    tests

--- TestThermo/Class1.cs
+++ TestThermo/Class1.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace TestThermo
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void ThermoCheck()
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
