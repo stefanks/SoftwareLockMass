274708b7aa7841b30fc4539b46dcba05c6d3ead6
stefan <solntsev@wisc.edu>
2016-06-28 13:55:26 -0500

    fixup

--- SoftwareLockMassCommandLine/Program.cs
+++ SoftwareLockMassCommandLine/Program.cs
@@ -4,19 +4,11 @@ namespace SoftwareLockMass
 {
     class Program
     {
-        private static string origDataFile;
-        private static string mzidFile;
-
-        public static string unimodLocation = @"unimod_tables.xml";
-        public static string psimodLocation = @"PSI-MOD.obo.xml";
-        public static string elementsLocation = @"elements.dat";
-        public static string uniprotLocation = @"ptmlist.txt";
-
         static void Main(string[] args)
         {
             //Console.Read();
-            origDataFile = args[0];
-            mzidFile = args[1];
+            string origDataFile = args[0];
+            string mzidFile = args[1];
             double intensityCutoff = 1e3;
             double toleranceInMZforSearch = 1e-2;
             if (args.Length > 2)
@@ -29,7 +21,7 @@ namespace SoftwareLockMass
             SoftwareLockMassParams a = SoftwareLockMassIO.IO.GetReady(origDataFile, P_outputHandler, P_progressHandler, P_watchHandler, mzidFile);
 
             SoftwareLockMassRunner.Run(a);
-            Console.Read();
+            //Console.Read();
         }
 
         private static void P_progressHandler(object sender, ProgressHandlerEventArgs e)
